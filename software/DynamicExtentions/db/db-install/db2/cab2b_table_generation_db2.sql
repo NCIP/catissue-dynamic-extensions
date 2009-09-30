98de1e368d7c8c08a38c5c99fa629e90507386fc
pavan_kalantri <pavan_kalantri>
2009-09-30 10:38:25 +0000

    changed the SQL
    SVN-Revision: 25745

--- software/DynamicExtentions/db/db-install/db2/cab2b_table_generation_db2.sql
+++ software/DynamicExtentions/db/db-install/db2/cab2b_table_generation_db2.sql
@@ -31,6 +31,6 @@ create table ID_TABLE(
     primary key (NEXT_ASSOCIATION_ID)
 );
 
-insert into ID_TABLE(NEXT_ASSOCIATION_ID) value(1);
+insert into ID_TABLE(NEXT_ASSOCIATION_ID) values(1);
 
 commit;
