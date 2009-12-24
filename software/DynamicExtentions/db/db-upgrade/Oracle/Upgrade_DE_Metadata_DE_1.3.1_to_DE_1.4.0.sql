52bd408e2a152948331f404d1af813016fff4a8a
rajesh_patil <rajesh_patil>
2009-12-24 14:51:08 +0000

    Build targets changes for DE bug fixes merging.
    SVN-Revision: 26123

---
+++ software/DynamicExtentions/db/db-upgrade/Oracle/Upgrade_DE_Metadata_DE_1.3.1_to_DE_1.4.0.sql
@@ -0,0 +1,23 @@
+ALTER TABLE DYEXTN_COMBOBOX ADD(NO_OF_COLUMNS number(10,0));
+
+
+create table DYEXTN_ABSTRACT_FORM_CONTEXT (
+	IDENTIFIER number(19,0) not null,
+	FORM_LABEL varchar(255),
+	CONTAINER_ID number(19,0),
+	HIDE_FORM number(1,0) default 0,
+	ACTIVITY_STATUS varchar(10),
+	primary key (IDENTIFIER)
+);
+
+create table DYEXTN_ABSTRACT_RECORD_ENTRY (
+	IDENTIFIER number(19,0) not null,
+	ABSTRACT_FORM_CONTEXT_ID number(19,0),
+	MODIFIED_DATE date,
+	MODIFIED_BY varchar(255),
+	ACTIVITY_STATUS varchar(10),
+	primary key (IDENTIFIER)
+);
+
+alter table DYEXTN_ABSTRACT_RECORD_ENTRY add constraint FKA74A5FE4DDFB1974 foreign key (ABSTRACT_FORM_CONTEXT_ID) references DYEXTN_ABSTRACT_FORM_CONTEXT (IDENTIFIER);
+alter table DYEXTN_CATEGORY add USER_ID number(19,0);
