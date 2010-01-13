6fa46b7e5db6a10ccdec63047dd213a0865a18e0
pavan_kalantri <pavan_kalantri>
2010-01-13 08:38:44 +0000

    changes for category creation test cases
    SVN-Revision: 26209

--- software/DynamicExtentions/script/do_checkout.sh
+++ software/DynamicExtentions/script/do_checkout.sh
@@ -1,7 +1,4 @@
-
+echo on ps6086 hudson username should contain @persistent.co.in  & for HSVN it should not thus hard coded the username
 echo ===============================================================================
-
-svn co $1 $2 --username $3 --password $4
-
-
+svn co $1 $2 --username nightlybuild@persistent.co.in --password Washu!@#
 echo ===============================================================================
