69ec1ca1edd0b566b0ce49b953b741642b4ebe77
pavan_kalantri <pavan_kalantri>
2010-01-13 16:06:10 +0000

    changes for category creation test cases
    SVN-Revision: 26217

--- software/DynamicExtentions/script/jbossStart.sh
+++ software/DynamicExtentions/script/jbossStart.sh
@@ -2,5 +2,5 @@ echo ---------------------------------------------starting  jboss server -------
 echo cd $1/bin
 cd $1/bin
 export JBOSS_HOME=$1
-ksh run.sh -b 0.0.0.0 >DEserver.log
+run.sh -b 0.0.0.0 >DEserver.log
 echo -------------------------------------NB part -2 finishes --------------------------------------------
