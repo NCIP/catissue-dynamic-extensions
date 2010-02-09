bce1537b5b9a1f5d2503719472408d95d87e3c99
rajesh_vyas <rajesh_vyas>
2010-02-09 10:14:16 +0000

    Task/Bug Id: Test case for XML to csv converter. Code Reviewed By: Kunal Kamble Technical Details:  Test case and related files. Unit tested(Y/N): Unit Test Case(s) Id/Name: Developer Notes: Test case and related files. Current Build++ Counts for this file : <B++ counts before the file is checked-in> PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 26431

--- software/DynamicExtentions/test/edu/common/dynamicextensions/category/TestXMLToCSVConverter.java
+++ software/DynamicExtentions/test/edu/common/dynamicextensions/category/TestXMLToCSVConverter.java
@@ -1,9 +1,12 @@
 
 package edu.common.dynamicextensions.category;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.io.Reader;
 
 import junit.framework.TestCase;
 
@@ -13,13 +16,15 @@ import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.wustl.common.util.logger.Logger;
 
 /**
- * Test class to test xml to csv conversion
+ * Test xml to csv conversion
  * @author rajesh_vyas
  *
  */
 public class TestXMLToCSVConverter extends TestCase
 {
-	private static final Logger logger = Logger.getCommonLogger(TestXMLToCSVConverter.class);
+
+	private static final Logger LOGGER = Logger.getCommonLogger(TestXMLToCSVConverter.class);
+
 	/**
 	 *
 	 */
@@ -28,23 +33,67 @@ public class TestXMLToCSVConverter extends TestCase
 		XMLToCSV xmlToCSV = new XMLToCSV();
 		try
 		{
-			URL resource = TestXMLToCSVConverter.class.getResource("Patient.xml");
+			File inputXMLFile = new File("software/DynamicExtentions/src/resources/xml/Patient.xml");
+			System.out.println("Input file: " + inputXMLFile.getAbsolutePath());
 			File outputDir = new File(System.getProperty("java.io.tmpdir"));
-			xmlToCSV.convertXMLs(new File(resource.getFile()), outputDir, null);
+			System.out.println(outputDir);
+			xmlToCSV.convertXMLs(inputXMLFile, outputDir, null);
+			File cSVFile = new File("software/DynamicExtentions/src/resources/csv/test_Patient.csv");
+			String expected = readXML(cSVFile);
+			String actual = readXML(new File(outputDir, "Patient.csv"));
+
+			assertEquals(expected, actual);
 		}
 		catch (DynamicExtensionsSystemException e)
 		{
-			logger.error(e.getStackTrace());
+			LOGGER.error(e.getMessage());
 		}
 		catch (IOException e)
 		{
-			logger.error(e.getStackTrace());
+			LOGGER.error(e.getMessage());
 		}
 		catch (SAXException e)
 		{
-			logger.error(e.getStackTrace());
+			LOGGER.error(e.getMessage());
 		}
-		//final String sampleContents = readSampleCSV();
-		//final XMLToCSVConverter xmlToCSVConverter = new XMLToCSVConverter();
+	}
+
+	/**
+	 * @param expectedFile
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	private String readXML(File expectedFile) throws FileNotFoundException, IOException
+	{
+		String textString = null;
+		BufferedReader xmlBufferedReader = null;
+		try
+		{
+			xmlBufferedReader = new BufferedReader(new FileReader(expectedFile));
+			StringBuilder stringBuilder = new StringBuilder();
+			char[] cbuf = new char[1024];
+			int read = xmlBufferedReader.read(cbuf);
+
+			while (read != -1)
+			{
+				stringBuilder.append(cbuf);
+				read = xmlBufferedReader.read(cbuf);
+			}
+			textString = stringBuilder.toString();
+		}
+		finally
+		{
+			close(xmlBufferedReader);
+		}
+
+		return textString;
+	}
+
+	private void close(Reader reader) throws IOException
+	{
+		if (reader != null)
+		{
+			reader.close();
+		}
 	}
 }
