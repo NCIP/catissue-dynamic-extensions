2c02bdba8c43bad4b451e25d29a352cf0b0b0cea
rajesh_vyas <rajesh_vyas>
2010-02-17 06:06:23 +0000

    Task/Bug Id: 16133 Code Reviewed By: Deepti Shelar Technical Details:  Changed Enum 'Width' to 'Columns'. And removed unnecessary Enums. Unit tested(Y/N):  N Unit Test Case(s) Id/Name: Developer Notes: Changed Enum 'Width' to 'Columns'. And removed unnecessary Enums. Current Build++ Counts for this file : <B++ counts before the file is checked-in> PMD - X     Checkstyle - X     Findbugs - X     CPD – X
    SVN-Revision: 26491

--- software/DynamicExtentions/src/java/edu/common/dynamicextensions/category/enums/TextFieldEnum.java
+++ software/DynamicExtentions/src/java/edu/common/dynamicextensions/category/enums/TextFieldEnum.java
@@ -1,128 +1,11 @@
 
 package edu.common.dynamicextensions.category.enums;
 
-import edu.common.dynamicextensions.domain.AttributeTypeInformation;
-import edu.common.dynamicextensions.domain.CategoryAttribute;
-import edu.common.dynamicextensions.domain.DoubleAttributeTypeInformation;
-import edu.common.dynamicextensions.domain.IntegerAttributeTypeInformation;
-import edu.common.dynamicextensions.domain.StringAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.userinterface.TextField;
-import edu.common.dynamicextensions.domaininterface.AttributeInterface;
-import edu.common.dynamicextensions.ui.util.ControlsUtility;
-import edu.common.dynamicextensions.util.parser.CategoryCSVConstants;
 
 public enum TextFieldEnum {
-	MAXLENGTH("width") {
 
-		/**
-		 * Returns String representation of max-length value for a control.
-		 */
-		public String getControlProperty(TextField control)
-		{
-			CategoryAttribute baseAbstractAttribute2 = (CategoryAttribute) control
-					.getBaseAbstractAttribute();
-			AttributeInterface attribute = baseAbstractAttribute2.getAttribute();
-
-			AttributeTypeInformation attributeTypeInformation = (AttributeTypeInformation) attribute
-					.getAttributeTypeInformation();
-
-			Integer maxLength = null;
-			if (attributeTypeInformation instanceof StringAttributeTypeInformation)
-			{
-				StringAttributeTypeInformation strAttributeTypeInformation = (StringAttributeTypeInformation) attributeTypeInformation;
-				maxLength = strAttributeTypeInformation.getSize();
-			}
-			else if (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
-			{
-				DoubleAttributeTypeInformation strAttributeTypeInformation = (DoubleAttributeTypeInformation) attributeTypeInformation;
-				maxLength = strAttributeTypeInformation.getDigits();
-			}
-			else if (attributeTypeInformation instanceof IntegerAttributeTypeInformation)
-			{
-				IntegerAttributeTypeInformation strAttributeTypeInformation = (IntegerAttributeTypeInformation) attributeTypeInformation;
-				maxLength = strAttributeTypeInformation.getDecimalPlaces();
-			}
-
-			return String.valueOf(maxLength);
-		}
-
-		/**
-		 * Sets String representation of maxlength value for a control.
-		 */
-		public void setControlProperty(TextField control, String propertyToBeSet)
-		{
-			CategoryAttribute baseAbstractAttribute2 = (CategoryAttribute) control
-					.getBaseAbstractAttribute();
-			AttributeInterface attribute = baseAbstractAttribute2.getAttribute();
-			AttributeTypeInformation attributeTypeInformation = (AttributeTypeInformation) attribute
-					.getAttributeTypeInformation();
-			if (attributeTypeInformation instanceof StringAttributeTypeInformation)
-			{
-				StringAttributeTypeInformation strAttributeTypeInformation = (StringAttributeTypeInformation) attributeTypeInformation;
-				strAttributeTypeInformation.setSize(Integer.valueOf(propertyToBeSet));
-			}
-			else if (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
-			{
-				DoubleAttributeTypeInformation strAttributeTypeInformation = (DoubleAttributeTypeInformation) attributeTypeInformation;
-				strAttributeTypeInformation.setDigits(Integer.valueOf(propertyToBeSet));
-			}
-			else if (attributeTypeInformation instanceof IntegerAttributeTypeInformation)
-			{
-				IntegerAttributeTypeInformation strAttributeTypeInformation = (IntegerAttributeTypeInformation) attributeTypeInformation;
-				strAttributeTypeInformation.setDecimalPlaces(Integer.valueOf(propertyToBeSet));
-			}
-		}
-	},
-	RANGEMIN("RANGEMIN") {
-
-		/**
-		 * Returns String representation of range-min value for a control.
-		 */
-		public String getControlProperty(TextField control)
-		{
-			boolean result = ControlsUtility.isRuleDefined(CategoryCSVConstants.MIN, control);
-			//ControlsUtility.
-			String ruleName = null;
-			if (result)
-			{
-				ruleName = CategoryCSVConstants.MIN;
-			}
-			return ruleName;
-		}
-
-		/**
-		 * Sets String representation of range-min value for a control.
-		 */
-		public void setControlProperty(TextField control, String propertyToBeSet)
-		{
-			ControlsUtility.defineRule(CategoryCSVConstants.MIN, control);
-		}
-	},
-	RANGEMAX("RANGEMAX") {
-
-		/**
-		 * Returns String representation of range-max value for a control.
-		 */
-		public String getControlProperty(TextField control)
-		{
-			boolean result = ControlsUtility.isRuleDefined(CategoryCSVConstants.MAX, control);
-			String ruleName = null;
-			if (result)
-			{
-				ruleName = CategoryCSVConstants.MAX;
-			}
-			return ruleName;
-		}
-
-		/**
-		 * Sets String representation of range-max value for a control.
-		 */
-		public void setControlProperty(TextField control, String propertyToBeSet)
-		{
-			ControlsUtility.defineRule(CategoryCSVConstants.MAX, control);
-		}
-	},
-	ISPASSWORD("ISPASSWORD") {
+	ISPASSWORD("isPassword") {
 
 		/**
 		 * Returns String representation of password value for a control.
@@ -145,7 +28,7 @@ public enum TextFieldEnum {
 			control.setIsPassword(Boolean.valueOf(propertyToBeSet));
 		}
 	},
-	ISURL("ISURL") {
+	ISURL("isURL") {
 
 		/**
 		 * Returns String representation of url value for a control.
