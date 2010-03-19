f38d4ea6106420f150244911a0c91db7d58854eb
pavan_kalantri <pavan_kalantri>
2010-03-19 06:54:50 +0000

    added the utility for csd
    SVN-Revision: 26620

---
+++ software/DynamicExtentions/src/java/edu/common/dynamicextensions/host/csd/util/CSDUtility.java
@@ -0,0 +1,13 @@
+
+package edu.common.dynamicextensions.host.csd.util;
+
+/**
+ * This class is specific to the CSd project. Any specific utility methods they need
+ * Can be put here & used by them.
+ * @author pavan_kalantri
+ *
+ */
+public class CSDUtility
+{
+
+}
