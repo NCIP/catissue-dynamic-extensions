bae8228958aab4f740bd774091d307e7d7957abc
chetan_pundhir <chetan_pundhir>
2010-03-26 14:37:05 +0000

    Added Test Case for CSDUtility Class Code Reviewed By: Pavan
    SVN-Revision: 26701

---
+++ software/DynamicExtentions/test/edu/common/dynamicextensions/host/csd/util/TestCSDUtility.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+package edu.common.dynamicextensions.host.csd.util;
+
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
+
+/**
+ * @author chetan_pundhir
+ *
+ */
+public class TestCSDUtility
+{
+
+	/**
+	 * Test merge string arrays.
+	 * @throws DynamicExtensionsSystemException 
+	 */
+	public void testGetAllCategoriesInfo() throws DynamicExtensionsSystemException
+	{
+		CSDUtility.getAllCategoriesInfo();
+	}
+}
