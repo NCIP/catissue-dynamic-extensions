0ece9782b8af0c81f6574c49f22a0b47dc5c43c2
kunal_kamble <kunal_kamble>
2010-04-01 11:13:31 +0000

    Bug 16524 -  Unable to connect 2 nodes in DAG
    SVN-Revision: 26758

--- software/DynamicExtentions/src/java/edu/wustl/cab2b/server/util/DynamicExtensionUtility.java
+++ software/DynamicExtentions/src/java/edu/wustl/cab2b/server/util/DynamicExtensionUtility.java
@@ -701,8 +701,8 @@ public final class DynamicExtensionUtility
 			final HibernateDAO hibernateDAO) throws DAOException
 	{
 		List<EntityGroupInterface> entityGroups = new ArrayList<EntityGroupInterface>();
-		Collection<EntityGroupInterface> allEntityGroups = hibernateDAO.executeNamedQuery(
-				"getAllEntityGroups", new HashMap<String, NamedQueryParam>());
+		Collection<EntityGroupInterface> allEntityGroups = hibernateDAO
+				.retrieve(EntityGroupInterface.class.getName());
 		for (EntityGroupInterface entityGroup : allEntityGroups)
 		{
 			if (isEntityGroupMetadata(entityGroup))
