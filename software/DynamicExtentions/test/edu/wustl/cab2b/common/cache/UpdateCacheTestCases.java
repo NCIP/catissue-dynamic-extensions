525a40c0ea35ef04094e19fb423f2783564c836a
Suhas Khot <suhas_khot@persistent.co.in>
2011-05-16 05:34:32 +0000

    Commented update cache test cases, since method regarding this are no longer exists
    SVN-Revision: 27716

--- software/DynamicExtentions/test/edu/wustl/cab2b/common/cache/UpdateCacheTestCases.java
+++ software/DynamicExtentions/test/edu/wustl/cab2b/common/cache/UpdateCacheTestCases.java
@@ -19,53 +19,53 @@ import edu.wustl.cab2b.server.cache.EntityCache;
 public class UpdateCacheTestCases extends DynamicExtensionsBaseTestCase
 {
 
-	public void testLockAndReleaseEntity()
-	{
-		String entityGroupName = "test";
-		try
-		{
-			//Step 1. Fetch a Entity Group.
-			EntityGroupInterface entityGroupInterface = EntityManager.getInstance()
-					.getEntityGroupByName(entityGroupName);
-			if (entityGroupInterface != null)
-			{
-				EntityCache cache = EntityCache.getInstance();
-				//Step 2. Refresh EntityCache for the Entity Group.
-				cache.refreshCache(entityGroupInterface);
-
-				//Step 3. Lock all Entities for the Entity Group.
-				cache.lockAllEntities(entityGroupInterface.getEntityCollection());
-
-				//step 4. Lock all containers for the Entity Group
-				cache.lockAllContainer(EntityGroupManagerUtil
-						.getAssociatedFormId(entityGroupInterface));
-				if (cache.entitiesInUse.isEmpty())
-				{
-					fail();
-				}
-
-				//Step 5. Release all Entities for the Entity Group.
-				cache.releaseAllEntities(entityGroupInterface.getEntityCollection());
-
-				//Step 5. Release all Containers for the Entity Group.
-				cache.releaseAllContainer(EntityGroupManagerUtil
-						.getAssociatedFormId(entityGroupInterface));
-				if (!cache.entitiesInUse.isEmpty())
-				{
-					fail();
-				}
-			}
-		}
-		catch (DynamicExtensionsSystemException e)
-		{
-			e.printStackTrace();
-			fail();
-		}
-		catch (DynamicExtensionsApplicationException e)
-		{
-			e.printStackTrace();
-			fail();
-		}
-	}
+//	public void testLockAndReleaseEntity()
+//	{
+//		String entityGroupName = "test";
+//		try
+//		{
+//			//Step 1. Fetch a Entity Group.
+//			EntityGroupInterface entityGroupInterface = EntityManager.getInstance()
+//					.getEntityGroupByName(entityGroupName);
+//			if (entityGroupInterface != null)
+//			{
+//				EntityCache cache = EntityCache.getInstance();
+//				//Step 2. Refresh EntityCache for the Entity Group.
+//				cache.refreshCache(entityGroupInterface);
+//
+//				//Step 3. Lock all Entities for the Entity Group.
+//				cache.lockAllEntities(entityGroupInterface.getEntityCollection());
+//
+//				//step 4. Lock all containers for the Entity Group
+//				cache.lockAllContainer(EntityGroupManagerUtil
+//						.getAssociatedFormId(entityGroupInterface));
+//				if (cache.entitiesInUse.isEmpty())
+//				{
+//					fail();
+//				}
+//
+//				//Step 5. Release all Entities for the Entity Group.
+//				cache.releaseAllEntities(entityGroupInterface.getEntityCollection());
+//
+//				//Step 5. Release all Containers for the Entity Group.
+//				cache.releaseAllContainer(EntityGroupManagerUtil
+//						.getAssociatedFormId(entityGroupInterface));
+//				if (!cache.entitiesInUse.isEmpty())
+//				{
+//					fail();
+//				}
+//			}
+//		}
+//		catch (DynamicExtensionsSystemException e)
+//		{
+//			e.printStackTrace();
+//			fail();
+//		}
+//		catch (DynamicExtensionsApplicationException e)
+//		{
+//			e.printStackTrace();
+//			fail();
+//		}
+//	}
 
 }
