c47b97b68c491e79333e344b5cfc353f48da3552
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2012-11-23 17:46:00 +0000

    added method for CSD
    SVN-Revision: 28626

--- software/DynamicExtentions/src/java/edu/common/dynamicextensions/host/csd/util/CSDUtility.java
+++ software/DynamicExtentions/src/java/edu/common/dynamicextensions/host/csd/util/CSDUtility.java
@@ -3,6 +3,7 @@ package edu.common.dynamicextensions.host.csd.util;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 import edu.common.dynamicextensions.entitymanager.AbstractBaseMetadataManager;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
@@ -21,6 +22,13 @@ public class CSDUtility
 	{
 		Collection categoryDetails = AbstractBaseMetadataManager.executeHQL("getAllCategoriesInfo",
 				new HashMap<String, NamedQueryParam>());
+		return categoryDetails;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Collection getAllUsersCategoriesInfo(Map<String, NamedQueryParam> param) throws DynamicExtensionsSystemException
+	{
+		Collection categoryDetails = AbstractBaseMetadataManager.executeHQL("getAllUsersCategoriesInfo",param);
 		return categoryDetails;
 	}
 }
